5626441dc3abb7c8f9d8f43e475620d8f5d58ecf
Eldher Hernández <eldher02@gmail.com>
2017-12-05 22:16:56 -0500

    Update IntraClases.r

--- IntraClases.r
+++ IntraClases.r
@@ -30,31 +30,18 @@ calcularIntraClase <- function(n){
   DT <- cbind(data, clust = results[[n]]$cluster)
   DT <- as.data.table(DT)
   
-  # results[[1]]$centers[1,][2] 2da componente del centro del primer cluster, del primer algoritmo
-  # results[[1]]$centers[2,] centro del segundo cluster con el primer algoritmo
-  
   print(paste0("-----",algos[[n]]))
 
-  data_c1 <- DT[clust==1 ,c(1,2,3), with = FALSE]  # tabla con solo datos, sin el ID cluster. Cluster 1
-  
-  cluster_1 <- apply(data_c1, 1, function(params) 
-    (params[1] - results[[1]]$centers[1,][1])^2 + 
-    (params[2] - results[[1]]$centers[1,][2])^2 +
-    (params[3] - results[[1]]$centers[1,][3])^2)
-  
+  data_c1 <- DT[clust==1 ,c(1:3), with = FALSE]  # tabla con solo datos, sin el ID cluster. Cluster 1
+  cluster_1 <- apply(data_c1, 1, function(params) (params - results[[1]]$centers[1,])^2)
   s1 <- sum(cluster_1)
 
   
-  data_c2 <- DT[clust==2 ,c(1,2,3), with = FALSE] # tabla con solo datos, sin el ID cluster. Cluster 2
-  
-  
-  cluster_2 <- apply(data_c2, 1, function(params) 
-    (params[1] - results[[1]]$centers[2,][1])^2 + 
-    (params[2] - results[[1]]$centers[2,][2])^2 +
-    (params[3] - results[[1]]$centers[2,][3])^2)
-  
+  data_c2 <- DT[clust==2 ,c(1:3), with = FALSE] # tabla con solo datos, sin el ID cluster. Cluster 2
+  cluster_2 <- apply(data_c2, 1, function(params) (params - results[[2]]$centers[1,])^2)
+
   s2 <- sum(cluster_2)
   print(paste0("Cluster1: ",s1," cluster2: ",s2," Suma: ",s1+s2))
 }
 
-exec <- sapply(c(1:4), calcularIntraClase)
+exec <- lapply(c(1:4), calcularIntraClase)
